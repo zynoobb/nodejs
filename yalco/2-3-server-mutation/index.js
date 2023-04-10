3b4c707eeeba5aed802235d382ad3e3d8d40703d
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-04-10 11:11:38 +0900

    Feat : 2-3 학습 완료

---
+++ yalco/2-3-server-mutation/index.js
@@ -0,0 +1,97 @@
+const database = require("./database");
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    teams: [Team]
+    team(id: Int): Team
+    equipments: [Equipment]
+    supplies: [Supply]
+  }
+  type Mutation {
+    deleteEquipment(id: String): Equipment
+    insertEquipment(
+      id: String
+      used_by: String
+      count: Int
+      new_or_used: String
+    ): Equipment
+    editEquipment(
+      id: String
+      used_by: String
+      count: Int
+      new_or_used: String
+    ): Equipment
+  }
+
+  type Team {
+    id: Int
+    manager: String
+    office: String
+    extension_number: String
+    mascot: String
+    cleaning_duty: String
+    project: String
+    supplies: [Supply]
+  }
+  type Equipment {
+    id: String
+    used_by: String
+    count: Int
+    new_or_used: String
+  }
+  type Supply {
+    id: String
+    team: Int
+  }
+`;
+const resolvers = {
+  Query: {
+    teams: () =>
+      database.teams.map((team) => {
+        team.supplies = database.supplies.filter((supply) => {
+          return supply.team === team.id;
+        });
+        return team;
+      }),
+    team: (parent, args, context, info) =>
+      database.teams.filter((team) => {
+        return team.id === args.id;
+      })[0],
+    equipments: () => database.equipments,
+    supplies: () => database.supplies,
+  },
+  Mutation: {
+    deleteEquipment: (parent, args, context, info) => {
+      const deleted = database.equipments.filter((equipment) => {
+        return equipment.id === args.id;
+      })[0];
+      database.equipments = database.equipments.filter((equipment) => {
+        return equipment.id !== args.id;
+      });
+      return deleted;
+    },
+
+    insertEquipment: (parent, args, context, info) => {
+      database.equipments.push(args);
+      return args;
+    },
+
+    editEquipment: (parent, args, context, info) => {
+      return database.equipments
+        .filter((equipment) => {
+          return equipment.id === args.id;
+        })
+        .map((equipment) => {
+          Object.assign(equipment, args);
+          return equipment;
+        })[0];
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
