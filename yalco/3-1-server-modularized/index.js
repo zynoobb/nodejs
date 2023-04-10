19e408c1f0563491523da935ff3c7482f8e7cd33
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-04-10 11:26:14 +0900

    Feat : 3-1 학습 완료

---
+++ yalco/3-1-server-modularized/index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require("apollo-server");
+const _ = require("lodash");
+
+// 불러와서 사용한다. nest와 크게 다르지 않음
+const queries = require("./typedefs-resolvers/_queries");
+const mutations = require("./typedefs-resolvers/_mutations");
+const equipments = require("./typedefs-resolvers/equipments");
+const supplies = require("./typedefs-resolvers/supplies");
+
+// 배열로 쿼리, 뮤테이션, 타입을 index로 가져와 사용한다.
+const typeDefs = [
+  queries, //
+  mutations, //
+  equipments.typeDefs, //
+  supplies.typeDefs, //
+];
+
+const resolvers = [
+  equipments.resolvers, //
+  supplies.resolvers, //
+];
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
