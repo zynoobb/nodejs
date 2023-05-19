ef553baf4d0b7d5a67e5e56d2e4c913e461c519b
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-05-19 14:34:24 +0900

    Refactor : typeDefs, resolver 위치 변경

--- yalco/3-4-arg-input-type/index.js
+++ yalco/3-4-arg-input-type/index.js
@@ -1,35 +1,5 @@
 const { ApolloServer } = require("apollo-server");
-
-const queries = require("./typedefs-resolvers/_queries");
-const mutations = require("./typedefs-resolvers/_mutations");
-const enums = require("./typedefs-resolvers/_enums");
-const equipments = require("./typedefs-resolvers/equipments");
-const softwares = require("./typedefs-resolvers/softwares");
-const supplies = require("./typedefs-resolvers/supplies");
-const givens = require("./typedefs-resolvers/givens");
-const tools = require("./typedefs-resolvers/tools");
-const people = require("./typedefs-resolvers/people");
-
-const typeDefs = [
-  queries,
-  mutations,
-  enums,
-  equipments.typeDefs,
-  softwares.typeDefs,
-  supplies.typeDefs,
-  givens.typeDefs,
-  tools.typeDefs,
-  people.typeDefs,
-];
-
-const resolvers = [
-  equipments.resolvers,
-  softwares.resolvers,
-  supplies.resolvers,
-  givens.resolvers,
-  tools.resolvers,
-  people.resolvers,
-];
+const { typeDefs, resolvers } = require("./typeDefsNresolver");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
